04374175fb239b00c6ed0ce1a67d4833ed720fd6
Ibrahima Koné <papuskone43@gmail.com>
2019-07-18 19:46:25 +0200

    installation de api rest django

--- Gestcom/models.py
+++ Gestcom/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+    #date = models.DateTimeField(default=timezone.now, 
+    #                            verbose_name="Date de parution")
